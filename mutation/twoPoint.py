6cdc43b88842f319da6c5ce70748d2c53b0b65db
Pawel <plpasza@gmail.com>
2021-11-01 15:59:00 +0100

    mutuation package

---
+++ mutation/twoPoint.py
@@ -0,0 +1,19 @@
+from chromosome import BinCromosome
+from .mutation import MutationStrategy
+from random import randrange
+
+
+class TwoPointStrategy(MutationStrategy):
+
+    def __init__(self):
+        super().__init__(True)
+
+    @staticmethod
+    def mutate(chromosome: BinCromosome):
+        points = []
+        for i in range(2):
+            mutation_point = randrange(0, chromosome.length, 1)
+            while mutation_point in points:
+                mutation_point = randrange(0, chromosome.length, 1)
+            points.append(mutation_point)
+            chromosome[mutation_point] = not chromosome[mutation_point]
