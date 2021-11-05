b91b1843c09b3f1e2192fec77c178be38261cf38
Pawel <plpasza@gmail.com>
2021-11-05 21:02:23 +0100

    added mutation and crossover combos

--- main.py
+++ main.py
@@ -23,10 +23,17 @@ binary = True
 width = 500
 height = 400
 
+population_size: QLineEdit
+chromosome_size: QLineEdit
+
 selectionStrategy: SelectionStrategy
 selectionParams: Dict = {}
 
 crossoverStrategy: CrossoverStrategy
+crossoverProbability: QLineEdit
+
+mutationStrategy: MutationStrategy
+mutationProbability: QLineEdit
 
 
 def main():
@@ -36,8 +43,9 @@ def main():
     #
     # gen.plot_solution()
 
-    population_size: QLineEdit
-    chromosome_size: QLineEdit
+    global population_size
+    global chromosome_size
+
     selectionStrategyCombo: QComboBox
 
     app = QApplication(sys.argv)
@@ -45,28 +53,83 @@ def main():
     mainWidget.setWindowTitle('Genetic algorithm')
     mainLayout = QGridLayout()
     mainWidget.setLayout(mainLayout)
+    mainWidget.setStyleSheet("border:1px solid rgb(0, 0, 0); ")
 
-    w, population_size = get_input_with_label('Population size', int, width=70)
+    w, population_size = get_input_with_label('Population size', int, width=50)
     mainLayout.addWidget(w, 0, 0)
-    w, chromosome_size = get_input_with_label('Chromosome size', int, width=70)
+    w, chromosome_size = get_input_with_label('Chromosome size', int, width=50)
     mainLayout.addWidget(w, 1, 0)
 
     mainLayout.addWidget(get_selection_strategy_widget(), 2, 0)
     mainLayout.addWidget(get_crossover_strategy_widget(), 3, 0)
+    mainLayout.addWidget(get_mutation_strategy_widget(), 4, 0)
 
-    mainWidget.resize(width, height)
+    # mainWidget.resize(width, height)
     mainWidget.show()
     sys.exit(app.exec())
 
 
-def get_crossover_strategy_widget():
-    global crossoverStrategy
-    crossoverWidget = QWidget()
+def get_mutation_strategy_widget():
+    global mutationStrategy
+    global mutationProbability
+    widget = QWidget()
+    layout = QGridLayout()
+    widget.setLayout(layout)
     combo = QComboBox()
     if binary:
-        combo.addItems(binarySelections)
+        combo.addItems(binaryMutations)
+        mutationStrategy = get_mutation(binaryMutations[0])
     else:
         raise NotImplemented
+
+    def onchange():
+        global mutationStrategy
+        mutationStrategy = get_mutation(combo.currentText())
+        print(mutationStrategy)
+
+    combo.currentIndexChanged.connect(onchange)
+    layout.addWidget(QLabel('Mutation strategy'), 0, 0)
+    layout.addWidget(combo, 0, 1)
+    w, mutationProbability = get_input_with_label(
+        'Crossover probability',
+        float,
+        content_margins=[0, 0, 0, 0],
+        width=50
+    )
+    layout.addWidget(w, 1, 0, 1, 2)
+    return widget
+
+
+
+def get_crossover_strategy_widget():
+    global crossoverStrategy
+    global crossoverProbability
+    crossoverWidget = QWidget()
+    crossoverLayout = QGridLayout()
+    crossoverWidget.setLayout(crossoverLayout)
+    combo = QComboBox()
+    if binary:
+        combo.addItems(binaryCrossovers)
+        crossoverStrategy = get_crossover(binaryCrossovers[0])
+    else:
+        raise NotImplemented
+
+    def onchange():
+        global crossoverStrategy
+        crossoverStrategy = get_crossover(combo.currentText())
+        print(crossoverStrategy)
+
+    combo.currentIndexChanged.connect(onchange)
+
+    crossoverLayout.addWidget(QLabel('Crossover strategy'), 0, 0)
+    crossoverLayout.addWidget(combo, 0, 1)
+    w, crossoverProbability = get_input_with_label(
+        'Crossover probability',
+        float,
+        content_margins=[0, 0, 0, 0],
+        width=50
+    )
+    crossoverLayout.addWidget(w, 1, 0, 1, 2)
 
     return crossoverWidget
 
@@ -77,15 +140,15 @@ def get_selection_strategy_widget():
     selectionStrategyCombo = QComboBox()
     if binary:
         selectionStrategyCombo.addItems(binarySelections)
+        selectionStrategy = get_selection(binarySelections[0])
     else:
         raise NotImplemented
-    selectionStrategy = get_selection(binarySelections[0])
 
     def change_selection_params():
         params = selectionStrategy.get_params_list()
         clearLayout(paramsWidget.layout())
         for key, val in params.items():
-            w, hook = get_input_with_label(key, val)
+            w, hook = get_input_with_label(key, val, content_margins=[0, 0, 0, 0], width=50)
             selectionParams[key] = hook
             paramsWidget.layout().addWidget(w)
 
@@ -104,8 +167,9 @@ def get_selection_strategy_widget():
     selectionLayout.addWidget(QLabel('Selection params'), 1, 0)
     paramsWidget = QWidget()
     paramsLayout = QVBoxLayout()
+    paramsLayout.setContentsMargins(0, 0, 0, 0)
     paramsWidget.setLayout(paramsLayout)
-    selectionLayout.addWidget(paramsWidget, 2, 0)
+    selectionLayout.addWidget(paramsWidget, 2, 0, 1, 2)
     change_selection_params()
     return selectionWidget
 
@@ -126,6 +190,8 @@ def get_input_with_label(label, input_type: type = None, **kwargs):
         inp.setValidator(QDoubleValidator())
     layout.addWidget(label)
     layout.addWidget(inp)
+    if kwargs.get('content_margins') and isinstance(kwargs.get('content_margins'), list):
+        layout.setContentsMargins(*kwargs.get('content_margins'))
 
     widg = QWidget()
     widg.setLayout(layout)
